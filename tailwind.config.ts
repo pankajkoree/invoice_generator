d0af6196db4907013a90838714879e8e61c99642
pankajkoree <pankajkori2057@gmail.com>
2024-12-16 23:20:57 +0530

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,11 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}', // Include your app directory
-    './components/**/*.{js,ts,jsx,tsx}', // Include any components
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
-export default config
+export default config;
