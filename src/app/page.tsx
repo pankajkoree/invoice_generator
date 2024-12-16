6968d11e675fe116d35f5560295323f3580b09af
pankajkoree <pankajkori2057@gmail.com>
2024-12-16 23:30:50 +0530

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <div className="relative flex xl:w-[70%] min-h-screen bg-black">
-      <h1 className="text-red-600">Invoice Generator</h1>
-      <h2>Free online invoice maker</h2>
-      <p>
+    <div className="relative flex flex-col xl:w-[70%] min-h-screen bg-pink-500 left-[15%] items-center">
+      <h1 className="text-red-600 text-6xl">Invoice Generator</h1>
+      <h2 className="text-3xl">Free online invoice maker</h2>
+      <p className="relative flex xl:w-[50%] text-center text-xl">
         Create professional invoices online in just a few minutes with our free
         invoice generator. Simply enter your billing details, customize the
         design, and download or print as a PDF. Save time and stay organized
