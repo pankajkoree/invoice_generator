10b69835ee9fb90574369436e1b408cac8f37f6f
pankajkoree <pankajkori2057@gmail.com>
2024-12-17 21:29:49 +0530

    sign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col xl:w-[70%] min-h-screen bg-purple-300 xl:left-[15%] gap-">
-      {/* div for */}
+    <div className="relative flex flex-col xl:w-[70%] min-h-screen bg-purple-300 xl:left-[15%] text-slate-800 gap-12">
+      {/* div for heading, phrase and explanation for invoice */}
       <div className="relative flex flex-col top-12 items-center gap-8">
-        <h1 className="text-red-600 text-6xl">Invoice Generator</h1>
-        <h2 className="text-3xl italic">Free online invoice maker</h2>
+        <h1 className="text-slate-700 text-6xl font-extrabold">
+          Invoice Generator
+        </h1>
+        <h2 className="text-4xl italic ">Free online invoice maker</h2>
         <p className="relative flex xl:w-[50%] text-center text-xl">
           Create professional invoices online in just a few minutes with our
           free invoice generator. Simply enter your billing details, customize
@@ -14,16 +16,19 @@ export default function Home() {
           organized with easy-to-use features for all your invoicing needs.
         </p>
       </div>
-      <div className="relative flex flex-col top-12 items-center">
+      {/* ❌ div for heading, phrase and explanation for invoice */}
+      {/* div for suggestion and buttons */}
+      <div className="relative flex flex-col top-12 items-center gap-8">
         <div>
-          <p>Choose based on your invoice type</p>
+          <p className="text-3xl">Choose based on your invoice type</p>
         </div>
-        <div>
+        <div className="relative flex gap-8">
           <Button>Classical</Button>
           <Button>Business</Button>
           <Button>Moveepix</Button>
         </div>
       </div>
+      {/* ❌ div for suggestion and buttons */}3
     </div>
   );
 }
