baf929bda79bee063252c614f6391ae5b6e0e7ab
pankajkoree <pankajkori2057@gmail.com>
2024-12-18 20:22:33 +0530

    classical

--- src/app/classical/page.tsx
+++ src/app/classical/page.tsx
@@ -1,7 +1,20 @@
 const Classical = () => {
   return (
-    <div>
-      <h1>Classical</h1>
+    <div className="relative flex flex-col xl:w-[70%] xl:min-h-screen xl:left-[15%] text-center text-slate-800 gap-12">
+      {/* heading */}
+      <div>
+        <h1>
+          Create <b>classical</b> invoice in few minutes
+        </h1>
+      </div>
+
+      {/* details filling div */}
+      <div>
+        <h2>Invoice</h2>
+      </div>
+
+      {/* save */}
+      <div>{/* div */}</div>
     </div>
   );
 };
