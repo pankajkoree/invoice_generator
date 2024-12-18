9db3d5fe88ad4a861520f70e64a700463a980fe5
pankajkoree <pankajkori2057@gmail.com>
2024-12-18 21:19:00 +0530

    classical

--- src/app/classical/page.tsx
+++ src/app/classical/page.tsx
@@ -3,7 +3,7 @@ import { Label } from "@/components/ui/label";
 
 const Classical = () => {
   return (
-    <div className="relative flex flex-col xl:w-[70%] xl:min-h-screen xl:left-[15%] text-center text-slate-800 gap-12">
+    <div className="relative flex flex-col xl:w-[60%] xl:min-h-screen xl:left-[20%] text-center text-slate-800 gap-12">
       {/* heading */}
       <div>
         <h1>
@@ -21,18 +21,24 @@ const Classical = () => {
         {/* logo, invoice no. and date */}
         <div>
           {/* date and invoice no. */}
-          <div>
-            <div className="relative flex">
-              <Label>Invoice No.</Label>
-              <Input />
+          <div className="flex flex-col gap-1">
+            <div className="relative flex items-center">
+              <Label htmlFor="invoice_number" className="xl:w-[20%] text-xl">
+                Invoice No.
+              </Label>
+              <Input id="invoice_number" className="xl:w-[30%]" />
             </div>
             <div className="relative flex">
-              <Label>Invoice Date</Label>
-              <Input type="date" />
+              <Label htmlFor="invoice_date" className="xl:w-[20%] text-xl">
+                Invoice Date
+              </Label>
+              <Input id="invoice_date" type="date" className="xl:w-[30%]" />
             </div>
           </div>
           {/* logo */}
-          <div></div>
+          {/* <div>
+            <input type="file" />
+          </div> */}
         </div>
 
         {/* logo for billed by and billed to */}
