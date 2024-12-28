b65f554b16e33073914d53719d6904bb605a0b3a
pankajkoree <pankajkori2057@gmail.com>
2024-12-28 19:23:23 +0530

    mailer

--- src/helper/mailer.ts
+++ src/helper/mailer.ts
@@ -37,6 +37,24 @@ export const sendMail = async ({ mail, mailType, userId }: sendMailOptions) => {
         pass: process.env.mailPass,
       },
     } as SMTPTransport.Options);
+
+    const mailOptions = {
+      from: "tcsuk1998@gmail.com",
+      to: mail,
+      subject: mailType === "VERIFY" ? "Verify your email" : "reset password",
+      html: ` <p>Click <a href="${
+        process.env.DOMAIN
+      }/verifyemail?token=${hashedToken}">here</a> to ${
+        mailType === "VERIFY" ? "verify your email" : "reset your password"
+      }
+        or copy and paste the link below in your browser. <br> ${
+          process.env.DOMAIN
+        }/verifyemail?token=${hashedToken}
+        </p>`,
+    };
+
+    const mailResponse = await transporter.sendMail(mailOptions);
+    return mailResponse;
   } catch (error) {
     if (error instanceof Error) throw new Error(error.message);
     else throw new Error("An unknown error occurred");
