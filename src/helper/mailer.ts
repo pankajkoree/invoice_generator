ffe0906f2b03e525ac4cdaf769de3355049df11b
pankajkoree <pankajkori2057@gmail.com>
2024-12-28 19:17:03 +0530

    mailer

--- src/helper/mailer.ts
+++ src/helper/mailer.ts
@@ -1,6 +1,7 @@
 import nodemailer from "nodemailer";
 import User from "@/models/userModel";
 import bcryptjs from "bcryptjs";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
 
 interface sendMailOptions {
   mail: string;
@@ -13,8 +14,29 @@ export const sendMail = async ({ mail, mailType, userId }: sendMailOptions) => {
     const hashedToken = await bcryptjs.hash(userId.toString(), 10);
 
     if (mailType === "VERIFY") {
-      await User.findByIdAndUpdate(userId);
+      await User.findByIdAndUpdate(userId, {
+        $set: {
+          verifyToken: hashedToken,
+          verifyTokenExpiry: Date.now() + 3600000,
+        },
+      });
+    } else if (mailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        $set: {
+          forgotPassword: hashedToken,
+          verifyPasswordTokenExpiry: Date.now() + 3600000,
+        },
+      });
     }
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.mailHost,
+      port: process.env.mailPort,
+      auth: {
+        user: process.env.mailUser,
+        pass: process.env.mailPass,
+      },
+    } as SMTPTransport.Options);
   } catch (error) {
     if (error instanceof Error) throw new Error(error.message);
     else throw new Error("An unknown error occurred");
