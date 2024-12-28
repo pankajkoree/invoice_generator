d4f31fd805371c6a494f1421262f33a0edc3aa14
pankajkoree <pankajkori2057@gmail.com>
2024-12-28 18:45:46 +0530

    mailer

--- src/helper/mailer.ts
+++ src/helper/mailer.ts
@@ -2,9 +2,19 @@ import nodemailer from "nodemailer";
 import User from "@/models/userModel";
 import bcryptjs from "bcryptjs";
 
-export const sendMail = async ({ mail, mailType, userId }) => {
+interface sendMailOptions {
+  mail: string;
+  mailType: string;
+  userId: string;
+}
+
+export const sendMail = async ({ mail, mailType, userId }: sendMailOptions) => {
   try {
     const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+    if (mailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId);
+    }
   } catch (error) {
     if (error instanceof Error) throw new Error(error.message);
     else throw new Error("An unknown error occurred");
