eaef125e746c31b2dd4c0f86649499d0b2ecda79
pankajkoree <pankajkori2057@gmail.com>
2024-12-30 01:21:57 +0530

    verifyEmail api

---
+++ src/app/api/users/verifyEmail/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+import {connect} from "@/database/connector"
+import User from "@/models/userModel"
+
+
+connect()
+
+export const POST = async(request:NextRequest) => {
+try {
+    
+} catch (error) {
+    
+}
+}
