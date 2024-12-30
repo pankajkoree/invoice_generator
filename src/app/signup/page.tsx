27344ef8f075cf2cbeed681435bcf503c46f1778
pankajkoree <pankajkori2057@gmail.com>
2024-12-30 21:32:07 +0530

    signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 const Signup = () => {
   return (
-    <div className="relative flex flex-col xl:w-[30%] xl:left-[35%] text-xl text-slate-600 items-center gap-8 p-4 border-4 border-white rounded-xl bg-purple-100">
+    <div className="relative flex flex-col xl:w-[22%] xl:left-[39%] text-slate-600 items-center gap-8 p-4 border-4 border-white rounded-xl bg-purple-100">
       {/* heading and suggestion caption */}
       <div className="relative flex flex-col text-center">
         <h1 className="text-2xl font-semibold text-slate-800">
@@ -21,17 +21,28 @@ const Signup = () => {
         {/* username */}
         <div className="relative flex flex-col gap-2">
           <Label className="text-[16px]">Username</Label>
-          <Input type="text" placeholder="enter your fullname" required />
+          <Input
+            className="relative flex border-2 border-gray-400 focus:border-purple-500 focus:border-2"
+            type="text"
+            placeholder="enter your fullname"
+            required
+          />
         </div>
         {/* email */}
         <div className="relative flex flex-col gap-2">
           <Label className="text-[16px]">Email</Label>
-          <Input type="text" placeholder="enter your email" required />
+          <Input
+            className="relative flex border-2 border-gray-400 focus:border-purple-500 focus:border-2"
+            type="text"
+            placeholder="enter your email"
+            required
+          />
         </div>
         {/* password */}
         <div className="relative flex flex-col gap-2">
           <Label className="text-[16px]">Password</Label>
           <Input
+            className="relative flex border-2 border-gray-400 focus:border-purple-500 focus:border-2"
             type="text"
             placeholder="must be at least 8 characters"
             minLength={8}
