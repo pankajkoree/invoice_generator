44e77bbd5e761221e51d2de248581d5af2e78027
pankajkoree <pankajkori2057@gmail.com>
2024-12-30 14:14:16 +0530

    verifyEmail api

--- src/app/api/users/verifyEmail/route.ts
+++ src/app/api/users/verifyEmail/route.ts
@@ -1,14 +1,52 @@
 import { NextRequest, NextResponse } from "next/server";
-import {connect} from "@/database/connector"
-import User from "@/models/userModel"
+import { connect } from "@/database/connector";
+import User from "@/models/userModel";
 
+connect();
 
-connect()
+export const POST = async (request: NextRequest) => {
+  try {
+    const requestBody = await request.json();
+    const { token } = requestBody;
 
-export const POST = async(request:NextRequest) => {
-try {
-    
-} catch (error) {
-    
-}
-}
+    if (!token) {
+      return NextResponse.json({ error: "Token is required" }, { status: 400 });
+    }
+
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "Invalid or token expired" },
+        { status: 400 }
+      );
+    }
+
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+
+    await user.save();
+
+    return NextResponse.json(
+      {
+        message: "Email verified successfully",
+        success: true,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error:
+          error instanceof Error ? error.message : "An unknown error occurred",
+      },
+      { status: 500 }
+    );
+  }
+};
