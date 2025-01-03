44a37813ca6960685eca497194b881cca19f391e
pankajkoree <pankajkori2057@gmail.com>
2025-01-03 13:23:42 +0530

    middleware

--- middleware.ts
+++ middleware.ts
@@ -8,5 +8,5 @@ export function middleware(request: NextRequest) {
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/about/:path*',
+  matcher: ['/','/classical','/business','/moveepix'],
 }
