d6c4fe5a62b9b36fad745b71822eaa87ad2e58e2
pankajkoree <pankajkori2057@gmail.com>
2025-01-08 19:16:47 +0530

    middleware

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,20 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  const token = request.cookies.get("token");
+  const path = request.nextUrl.pathname;
+
+  if (path === "/carts") {
+    if (token?.value) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+
   return NextResponse.redirect(new URL("/", request.url));
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/", "/classical", "/business", "/moveepix"],
+  matcher: ["/", "/carts", "/login", "/buyNow"],
 };
