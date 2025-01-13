7f9c247f19f9585fea39f2b5a0707a5e9c063d92
pankajkoree <pankajkori2057@gmail.com>
2025-01-13 18:43:43 +0530

    ..

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,7 @@ export function middleware(request: NextRequest) {
 
   if (path === "/classical") {
     if (token?.value) {
-      return NextResponse.next().next();
+      return NextResponse.next();
     } else {
       return NextResponse.redirect(new URL("/login", request.url));
     }
